db051041a33a3159fce08f83ba4616800ab118eb
eli-temanson <eli.temanson@outlook.com>
2023-03-01 11:07:34 -0500

    backing up previous work

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Input files for FRESCO CRC-DWBA Calculations
+
+## To Do
+Make a python program to clean all the fort files
---
+++ scripts/DWBA_WF.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import interpolate, integrate
+import mpmath as mp
+import numpy as np
+
+HBARC=197.3269804  #MeV*fm
+
+mu=901.909/931.494 # amu 
+Zp=1
+Zt=13
+
+E=0.4138 # MeV
+k=0.218735*np.sqrt(mu*E)
+print('wave number, k = ',k)
+eta=0.1574854*Zp*Zt*np.sqrt(mu/E)
+print('sommerfeld parameter, coulomb eta = ', eta)
+
+r0=1.25
+ChannelRadius=r0*(1 + 25**(1.0/3.0))
+print('channel radius = ', ChannelRadius, ' (fm)')
+
+print('wavelength to sie ratio = ', k*ChannelRadius)
+
+def regCWF(l,eta,z):
+    func = np.frompyfunc(mp.coulombf, 3, 1)
+    return func(l,eta,z)
+
+def irregCWF(l,eta,z): 
+    func = np.frompyfunc(mp.coulombg, 3, 1)
+    return func(l,eta,z)
+
+wave_function = pd.read_csv('fort.58',sep='\s+',header=None,skiprows=1,skipfooter=1,engine='python')
+#print(df['r'])
+#r = df['r'].to_numpy
+#print(r)
+#U = np.genfromtxt('fort.58',delimiter='\s+',skip_header=1,skip_footer=1)
+#print(U)
+
+#for i in wave_function[0].size()
+#    x = i
+#    y = wave_function*i**2
+
+#norm = interpolate.UnivariateSpline(x,y).integral(0,30)
+norm = 1
+print('norm = ',norm)
+
+wave_function[1] = wave_function[1]/np.sqrt(norm)
+wf = interpolate.CubicSpline(wave_function[0], wave_function[1])
+
+reduced_width_sqr = 0.5*ChannelRadius*wf(ChannelRadius)**2
+print('Dimenisonless single-particle reduced width = ', reduced_width_sqr)
+
+#penetrability = 1.53953E-5
+penetrability = k*ChannelRadius/(regCWF(0,eta,k*ChannelRadius)**2 + irregCWF(0,eta,k*ChannelRadius)**2)
+print('penetrability = ', penetrability)
+
+print('Single-Particle Width = ', reduced_width_sqr *penetrability* 2*HBARC**2/(931.494*mu*ChannelRadius**2), ' (MeV)')
+
+
+plt.plot(wave_function[0], wf(wave_function[0])**2)
+plt.axvline(x=ChannelRadius,color='r',linestyle='--')
+plt.axvline(x=4.31,color='k',linestyle='--')
+plt.axvline(x=6.37,color='k',linestyle='--')
+plt.xlim(0,20)
+plt.ylabel('$u(r)^{2}$')
+plt.xlabel('Radius (fm)')
+#plt.show()
+
+
+    
